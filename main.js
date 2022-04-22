7eea4cd3d5727c96652699597f0e280aa9ee9043
arcox80 <drew.r.cox@gmail.com>
2022-04-22 12:18:48 -0400

    Added books array with some examples

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 // write your code here to make the tests pass
 const Library = function () {
+  const books = [
+    { title: 'The Tipping Point',
+      author: 'Malcolm Gladwell',
+      checkedOut: true
+    },
+    {
+      title: 'Generation Kill',
+      author: 'Evan Wright',
+      checkedOut: false
+    }
+  ];
   const addBook = function (book) {};
   
   return {
