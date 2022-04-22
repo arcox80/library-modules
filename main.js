51753f4d163d8b80462231f6fe3ecaa69e2687fd
arcox80 <drew.r.cox@gmail.com>
2022-04-21 21:55:59 -0400

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // write your code here to make the tests pass
+const Library = function () {
+  const addBook = function (book) {};
+  
+  return {
+    addBook: addBook
+  };
+};
+const Book = function(title, author) {
+  const bookObj = {
+    title: title,
+    author: author,
+    checkedOut: false
+  };
+};
